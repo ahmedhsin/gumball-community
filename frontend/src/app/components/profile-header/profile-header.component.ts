e0f2e69456297d4058b67bc809a77e6204b51bf2
Ahmed Mubarak <ahmedmubarak.hsin@gmail.com>
2024-09-13 17:57:49 +0300

    add profile page - with issues

--- frontend/src/app/components/profile-header/profile-header.component.ts
+++ frontend/src/app/components/profile-header/profile-header.component.ts
@@ -2,10 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-profile-header',
-  standalone: true,
-  imports: [],
   templateUrl: './profile-header.component.html',
-  styleUrl: './profile-header.component.css'
+  styleUrls: ['./profile-header.component.css']
 })
 export class ProfileHeaderComponent {
   name: string  = 'username';
