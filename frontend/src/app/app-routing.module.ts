ae3df91dbbc75b4aa6e968607a6757ff6daa617c
hala912 <hala19717131@gmail.com>
2024-09-15 00:01:54 +0300

    some design edits

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -20,7 +20,7 @@ const routes: Routes = [
         component: ProfileComponent
       }
     ]
-    
+
   }
   ,{
     path :'login',
--- frontend/src/app/components/side-nav/side-nav.component.ts
+++ frontend/src/app/components/side-nav/side-nav.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-side-nav',
   templateUrl: './side-nav.component.html',
   styleUrls: ['./side-nav.component.css']
 })
-export class SideNavComponent {
+export class SideNavComponent implements OnInit {
 
+  currentRoute: string = '';
+
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+    // Subscribe to router events to get the current route
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.currentRoute = event.urlAfterRedirects;
+      }
+    });
+  }
+
+  isActive(route: string): boolean {
+    return this.currentRoute === route;
+  }
 }
