e0e769a02a4a9860728643e76f7130a61123fa34
Ahmed Mubarak <ahmedmubarak.hsin@gmail.com>
2024-09-14 22:09:23 +0300

    handle reactions

--- frontend/src/app/components/react/react.component.ts
+++ frontend/src/app/components/react/react.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-react',
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./react.component.css']
 })
 export class ReactComponent {
-
+  @Input() setReact:any;
+  @Input() post:any;
+  
 }
