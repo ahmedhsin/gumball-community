5e12b45c8ff6b303e5ba46edc9960ae998963731
Ahmed Mubarak <ahmedmubarak.hsin@gmail.com>
2024-09-15 13:32:05 +0300

    fix styles issue

--- frontend/src/app/components/chats-box/chats-box.component.ts
+++ frontend/src/app/components/chats-box/chats-box.component.ts
@@ -6,5 +6,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./chats-box.component.css']
 })
 export class ChatsBoxComponent {
-
+  users = [
+    {
+      name:"Ahmed Mubarak",
+      img: "assets/images/profile.png",
+      id:1
+    },
+    {
+      name:"Ahmed Mubarak 2",
+      img: "assets/images/profile.png",
+      id:2
+    },
+    {
+      name:"Ahmed Mubarak 3",
+      img: "assets/images/profile.png",
+      id:3
+    },
+    {
+      name:"Ahmed Mubarak 4",
+      img: "assets/images/profile.png",
+      id:4
+    },
+    {
+      name:"Ahmed Mubarak 5",
+      img: "assets/images/profile.png",
+      id:5
+    },
+    {
+      name:"Ahmed Mubarak 6",
+      img: "assets/images/profile.png",
+      id:6
+    }
+  ]
 }
