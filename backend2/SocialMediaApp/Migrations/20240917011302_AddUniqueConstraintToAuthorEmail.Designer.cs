161065cd66125250f5d504d03b27ed365e159977
Ahmed Mubarak <ahmedmubarak.hsin@gmail.com>
2024-09-17 04:14:59 +0300

    missing constraint

--- backend2/SocialMediaApp/Migrations/20240917011302_AddUniqueConstraintToAuthorEmail.Designer.cs
+++ backend2/SocialMediaApp/Migrations/20240917011302_AddUniqueConstraintToAuthorEmail.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SocialMediaApp;
 
@@ -11,9 +12,11 @@ using SocialMediaApp;
 namespace SocialMediaApp.Migrations
 {
     [DbContext(typeof(SocialMediaContext))]
-    partial class SocialMediaContextModelSnapshot : ModelSnapshot
+    [Migration("20240917011302_AddUniqueConstraintToAuthorEmail")]
+    partial class AddUniqueConstraintToAuthorEmail
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- backend2/SocialMediaApp/Models/Author.cs
+++ backend2/SocialMediaApp/Models/Author.cs
@@ -1,12 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
 using static System.Net.Mime.MediaTypeNames;
 
 namespace SocialMediaApp.Models
 {
+	[Index(nameof(Email), IsUnique = true)]
 	public class Author
 	{
 		public int Id { get; set; }
 		public string FirstName { get; set; }
 		public string LastName { get; set; }
+		
+		[Required]
+		[EmailAddress]
 		public string Email { get; set; }
 		public string ProfileImageUrl { get; set; }
 		public string Password { get; set; }
